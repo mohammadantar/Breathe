e858d4357f04c194e5428b46b5f36917e29cbba8
mohammadantar <mohammadiutm@gmail.com>
2026-05-01 19:07:54 +0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,9 @@
+export const db = {
+    users: [],
+    scores: [],
+    memories: [
+        "watching the kettle boil and feeling a moment of complete peace",
+        "the smell of rain on a quiet Sunday morning",
+        "when your dog curled up next to you while you read"
+    ]
+};
